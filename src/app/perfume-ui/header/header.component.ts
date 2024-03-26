9394d107687cc9f76e7f9bc8ae2c413cf6139c00
AWEI <challengewei0625@gmail.com>
2024-03-26 09:33:20 +0800

    UI 切版完成

--- src/app/perfume-ui/header/header.component.ts
+++ src/app/perfume-ui/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+ isShowMenu:boolean = false; //預設先關閉選單
 
+ toggleMenu(){
+  this.isShowMenu = !this.isShowMenu; //切換選單的顯示狀態
+ }
 }
--- src/app/perfume-ui/perfume-ui.component.ts
+++ src/app/perfume-ui/perfume-ui.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-perfume-ui',
   templateUrl: './perfume-ui.component.html',
-  styleUrls: ['./perfume-ui.component.css']
+  styleUrls: ['./perfume-ui.component.scss']
 })
 export class PerfumeUIComponent {
 
